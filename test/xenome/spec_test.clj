2819aec510f0f11bcbdbef603b1a847bf45526d5
Calvin Sauer <calvin.j.sauer@gmail.com>
2017-06-21 00:00:34 -0400

    Simplify redundant tests

--- test/xenome/spec_test.clj
+++ test/xenome/spec_test.clj
@@ -1,17 +1,11 @@
 (ns xenome.spec-test
   (:require [clojure.test :refer :all]
             [clojure.spec.alpha :as s]
-            [clojure.test.check :as tc]
             [clojure.test.check.properties :as prop]
             [clojure.test.check.clojure-test :refer [defspec]]
-            [xenome.spec :as xs]
-            [xenome.validations :as v]))
+            [xenome.spec :as xs]))
 
-(defspec xenome-spec 50
+(defspec xenome-spec
   (prop/for-all
    [x (xs/xenome-gen)]
-   (and
-    (s/valid? ::xs/xenome x)
-    (v/edges-are-a-subset-of-nodes? x)
-    (v/sum-in-out-count-lte-node-count? x)
-    (v/innovations-are-distinct? x))))
+   (s/valid? ::xs/xenome x)))
